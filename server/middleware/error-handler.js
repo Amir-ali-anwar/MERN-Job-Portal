40f6bc0210028af16d019ad59a719c3f5dfd5dfd
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-07-10 23:07:41 +0500

    working on modifying the error handling middleware

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -3,7 +3,19 @@ import { StatusCodes } from "http-status-codes";
 const errorHandlerMiddleware = (err, req, res, next) => {
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "something went wrong",
   };
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    // defaultError.msg = err.message;
+    // defaultError.msg = Object.values(err.message)
+    //   .map((item) => item.message)
+    //   .join(",");
+  }
+  // res.status(defaultError.statusCode).json({ msg: defaultError.msg });
   res.status(defaultError.statusCode).json({ msg: err });
 };
 export default errorHandlerMiddleware;
