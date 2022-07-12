2bfcc22091a877c8192bb3b72a6cff755ec70503
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-07-12 14:32:41 +0500

    server setup completed

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,4 +1,6 @@
 import { StatusCodes } from "http-status-codes";
+import mongoose from "mongoose";
+import moment from "moment";
 import {
   BadRequestError,
   UnAuthenticatedError,
@@ -25,8 +27,43 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "job delete successfully" });
 };
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.UserID });
-  res.status(StatusCodes.OK).json({ jobs, nbhits: jobs.length, numofPages: 1 });
+  const { status, jobType, sort, search } = req.query;
+  const queryObject = {
+    createdBy: req.user.UserID,
+  };
+  if (status !== "all") {
+    queryObject.status = status;
+  }
+  if (jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+  // chain sort conditions
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  let result = Job.find(queryObject);
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params;
@@ -46,7 +83,47 @@ const updateJob = async (req, res) => {
   await existingJob.save();
   res.status(StatusCodes.OK).json({ existingJob });
 };
-const showStats = (req, res) => {
-  res.send("showStats");
+const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.UserID) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+  const defaultStatus = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+  const monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.UserID) } },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { month: "$createdA" },
+          count: { $sum: 1 },
+        },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { month, year },
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year - 1)
+        .format();
+      return { date, count };
+    })
+    .reverse();
+  res.status(StatusCodes.OK).json({ defaultStatus, monthlyApplications });
 };
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
