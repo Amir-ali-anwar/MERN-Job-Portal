f5a1162f3cfb549f8e2f6b62d841c883fbc8f2ea
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-07-15 09:51:17 +0500

    added small sidebar and added links file for smallsidear and bigsidebar

--- client/src/components/SmallSideBar.js
+++ client/src/components/SmallSideBar.js
@@ -1,7 +1,32 @@
+import { FaTimes } from "react-icons/fa";
 import React from "react";
+import Logo from "./Logo";
+// import NavLinks from "./NavLinks";
+import { NavLink } from "react-router-dom";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { Button } from "../components/";
 const SmallSideBar = () => {
-  return <Wrapper>SmallSideBar</Wrapper>;
+  const [toggleSideBar, SettoggleSideBar] = React.useState(false);
+  const sideBarToggleHandler = () => {
+    SettoggleSideBar(!toggleSideBar);
+  };
+  return (
+    <Wrapper>
+      <div
+        className={`sidebar-container ${toggleSideBar ? "show-sidebar" : ""}`}
+      >
+        <div className="content">
+          <Button
+            type="button"
+            className="close-btn"
+            handleChange={sideBarToggleHandler}
+          >
+            <FaTimes />
+          </Button>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSideBar;
