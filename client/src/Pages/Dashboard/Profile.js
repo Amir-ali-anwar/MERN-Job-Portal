70c3990b17a55e8005ed45a92ab767e4a369bda8
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-07-16 13:09:13 +0500

    working on profile page

--- client/src/Pages/Dashboard/Profile.js
+++ client/src/Pages/Dashboard/Profile.js
@@ -1,30 +1,59 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { displayAlert, clearAlert } from "../../features/user/userSlice";
 import { Button, FormRow, Alert } from "../../components";
 const Profile = () => {
-  const inputhandler = () => {
-    console.log("clicked");
+  const { isLoading, showAlert, user } = useSelector((store) => store.user);
+
+  const dispatch = useDispatch();
+  const [values, SetValues] = React.useState({
+    name: user.name || "",
+    email: user.email || "",
+    lastName: user.lastName || "",
+    location: user.location || "",
+  });
+  console.log(values);
+  const inputhandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log("name", name);
+    console.log("value", value);
+    SetValues({ ...values, [name]: value });
   };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit clicked");
+    const { name, email, lastName, location } = values;
+    if (!name || !email || !lastName || !location) {
+      dispatch(displayAlert());
+      return;
+    }
+  };
+  setTimeout(() => {
+    dispatch(clearAlert());
+  }, 3000);
   return (
     <Wrapper>
-      <form className="form">
+      <form className="form" onSubmit={submitHandler}>
         <h3>Profile</h3>
-        {/* {data.showAlert && <Alert />} */}
+        {showAlert && <Alert />}
         <div className="form-center">
           <FormRow
             name="name"
-            labelText=" Name"
+            labelText="Name"
             className="form-input"
             labelClass="form-label"
             handleChange={inputhandler}
+            value={values.name}
           />
           <FormRow
             name="Last Name"
-            labelText=" Last Name"
+            labelText="Last Name"
             className="form-input"
             labelClass="form-label"
             handleChange={inputhandler}
+            value={values.lastName}
           />
           <FormRow
             name="email"
@@ -33,6 +62,7 @@ const Profile = () => {
             className="form-input"
             labelClass="form-label"
             handleChange={inputhandler}
+            value={values.email}
           />
           <FormRow
             name="text"
@@ -40,8 +70,12 @@ const Profile = () => {
             className="form-input"
             labelClass="form-label"
             handleChange={inputhandler}
+            value={values.location}
           />
-          <Button className="btn btn-block">save changes</Button>
+          <Button className="btn btn-block" disabled={isLoading}>
+            {" "}
+            {isLoading ? "Please Wait..." : "save changes"}
+          </Button>
         </div>
       </form>
     </Wrapper>
