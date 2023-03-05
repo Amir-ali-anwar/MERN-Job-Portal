e4acedc6e21084d0d93ff4c4d21e55607bcbeec2
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-05 19:48:33 +0500

    little bugfix on authcontroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,14 +2,17 @@ import User from "../model/User.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 const register = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { name, email, password,confirmpass } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all the values");
   }
-  const alreadyExist= User.findOne(email)
+  const alreadyExist= await User.findOne({email})
   if(alreadyExist){
     throw new BadRequestError("Email already in use");
   }
+  if(password !== confirmpass){
+    throw new BadRequestError("Password and Confirm password should match");
+}
   const user = await User.create(req.body);
   const token = user.CreateJWT();
   return res.status(StatusCodes.CREATED).json({
